911f4c4a65e088fd1d67b49d03aca178975140c4
정승원(SeungWon.Jeong)/OnC <seungwon.jeong@wesang.com>
2024-07-12 20:29:36 +0900

    article test 생성

---
+++ app/tests/test_create_article.py
@@ -0,0 +1,21 @@
+from tortoise.contrib.test import TestCase
+
+from app.models.article import Article
+
+
+class TestCreateArticle(TestCase):
+
+    async def test_create_article(self) -> None:
+        # Given
+        author = "test_author"
+        title = "test_title"
+        body = "test_content"
+
+        # When
+        await Article.create(id="test_id", author=author, title=title, body=body)
+
+        # Then
+        article = await Article.get(title=title)
+        assert article.author == author
+        assert article.title == title
+        assert article.body == body
